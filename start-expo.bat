6e534b3979359d5207003f494e26d514ef0d39d8
AdibaFaiyaz <adibafaiyaz16@gmail.com>
2025-06-17 09:42:01 +0530

    mEow

--- start-expo.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set ANDROID_HOME=C:\Users\adiba\AppData\Local\Android\Sdk
-set PATH=%PATH%;%ANDROID_HOME%\platform-tools;%ANDROID_HOME%\tools;%ANDROID_HOME%\tools\bin
-cd /d "d:\vsc\AppDev\Meow"
-npx expo start
